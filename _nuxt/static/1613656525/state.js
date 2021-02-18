2f09db10e014a3b500ab76b93ce39c1db6202678
waseber <yvesgebrer@gmail.com>
2021-02-18 08:56:22 -0500

    whatevs

---
+++ _nuxt/static/1613656525/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613656525",layout:"default",error:a,serverRendered:true,routePath:b,config:{googleAnalytics:{id:"UA-189838966-1"},app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
